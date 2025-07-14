48326f3417632d9ed74cb026e3340ad0fecd801e
Aysenursvs <sivasligiln@gmail.com>
2025-07-14 12:40:40 +0300

    modified parser function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from reader import read_static_log_file
+from parser import parse_log_line
 
 log_lines = read_static_log_file('/home/aysenur/projects/nginx_analyzer/nginx-access-example.log')
 
 for line in log_lines:
-    print(line)
+    parsed_line = parse_log_line(line)
+    if parsed_line:
+        print(parsed_line)
--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+import re
+
 
 def parse_log_line(line):
     """
@@ -6,12 +8,24 @@ def parse_log_line(line):
     :param line: A line from the log file
     :return: A dictionary containing the parsed information
     """
-    # Example: 127.0.0.1 - - [01/Jan/2020:00:00:01 +0000] "GET /index.html HTTP/1.1" 200 1234
+    # Example: 172.18.0.1 - - [14/Jul/2025:07:53:41 +0000] "GET /auth/login-page HTTP/1.1" 200 2104 "http://localhost/" "Googlebot/2.1 (+http://www.google.com/bot.html)"
     try:
-        ip, _, _, datetime, request, status, size, referer, user_agent = line.split(" ", 8)
+        match = re.match(r'(\S+) - - \[([^\]]+)\] "([^"]+)" (\d+) (\d+) "([^"]*)" "([^"]*)"', line)
+        if not match:
+            return None
+        ip = match.group(1)
+        datetime = match.group(2).split(' ')[0]
+        utc = match.group(2).split(' ')[1] if ' ' in match.group(2) else ''
+        request = match.group(3)
+        status = match.group(4)
+        size = match.group(5)
+        referer = match.group(6)
+        user_agent = match.group(7)
+        
         return {
             "ip": ip,
             "datetime": datetime,
+            "utc": utc,
             "request": request,
             "status": status,
             "size": size,
