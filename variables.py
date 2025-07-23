69bed7d6f5a6a3761c549f4e17b427e02f17ccb3
Aysenursvs <sivasligiln@gmail.com>
2025-07-23 11:30:27 +0300

    variables are updated

--- variables.py
+++ variables.py
@@ -1,9 +1,25 @@
+## main.py ##
+#File paths:
+
+#Source files are static log files. that means new log lines are not added to them.
+#Target files are dynamic log files. that means new log lines are added to them.
+source_file_path_real = '/home/aysenur/projects/nginx_analyzer/nginx.vhost.access.log'
+target_file_path_real = '/home/aysenur/projects/nginx_analyzer/nginx-access-example.log'
+
+source_file_path_example = '/home/aysenur/projects/nginx_analyzer/nginx-source-log.log'
+target_file_path_example = '/home/aysenur/projects/nginx_analyzer/nginx-target-log.log'
+
+#load_ip_location_cache function loads IP location data from this file if it exists.
+ip_location_cache_file_path = '/home/aysenur/projects/nginx_analyzer/ip_location_cache_prefix.json'
+#load_prefix_counter function loads prefix counter data from this file if it exists.
+prefix_counter_file_path = '/home/aysenur/projects/nginx_analyzer/prefix_counter.json'
+
+#save_ip_data_to_file function saves ip data to this file
+log_results_file_path = '/home/aysenur/projects/nginx_analyzer/real_logs/ip_datas_real_dynamic_location.json'
+
+
+
 ### for updater functions
-
-bot_risk_score = 50
-suspicious_risk_score = 20
-rate_limit_risk_score = 20
-
 block_risk_score = 80
 review_risk_score = 50
 
